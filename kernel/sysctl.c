8d8b05fcb563b24d5cda74baf8c49ab3d33fdb7d
Tsu-ba-me <ynho.li.aa.e@gmail.com>
2020-03-31 15:20:19 -0400

    chore: patch kernel/sysctl.c

--- kernel/sysctl.c
+++ kernel/sysctl.c
@@ -52,6 +52,7 @@
 #include <linux/slow-work.h>
 #include <linux/perf_event.h>
 #include <linux/kprobes.h>
+#include <linux/dynaccel.h>
 #include <linux/kmod.h>
 #include <linux/capability.h>
 
@@ -1223,6 +1224,15 @@ static struct ctl_table kern_table[] = {
 		.proc_handler	= &proc_dointvec,
 	},
 #endif
+	{
+		.ctl_name       = CTL_UNNUMBERED,
+		.procname       = "accel",
+		.data           = &speedup_ratio,
+		.maxlen         = sizeof(int),
+		.mode           = 0644,
+		.proc_handler   = &proc_dointvec,
+	},
+
 /*
  * NOTE: do not add new entries to this table unless you have read
  * Documentation/sysctl/ctl_unnumbered.txt
